8b3fbd5bcb9ef91e639218e82e213087bd1694a6
qw2314fqe <492731664@qq.com>
2024-06-02 23:44:16 +0800

    注释说明

---
+++ helloworld.py
@@ -0,0 +1,28 @@
+#include<unistd.h>
+#include"rclcpp/rclcpp.hpp"   //ros2 C++接口
+
+
+class HelloWorldNode:public rclcpp::Node
+{
+public:
+    HelloWorldNode():Node("node_helloworld") 
+    {
+        while(rclcpp::ok())
+        {
+            RCLCPP_INFO(this->get_logger(),"hello world! ros2");
+            sleep(1);
+        }
+    }
+};
+
+int main(int argc,char*argv[])
+{
+
+    rclcpp::init(argc,argv);
+
+    rclcpp::spin(std::make_shared<HelloWorldNode>());
+
+    rclcpp::shutdown();
+    return 0;
+}
+
